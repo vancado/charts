7e1dad759824918be28a9d45a7e97c3c1548d2a1
Johannes Böttcher <johannes.boettcher@vancado.de>
2026-02-05 12:33:10 +0100

    [TASK] Update for TYPO3 v13
    Resolves: #SWS-522

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state'        => 'stable',
     'constraints'  => [
         'depends'  => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.4.99',
         ],
         'suggests' => [
-            'spreadsheets' => '3.3.0-4.99.99',
+            'spreadsheets' => '3.3.0-5.99.99',
         ],
     ],
     'autoload'     => [
